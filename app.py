192b934a5a3ff02a174105caeb23098965bb2604
ashwini_dubile <ashwinidubile96@gmail.com>
2025-09-07 15:34:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return "Hello, this is my first CI/CD project with Jenkins and Docker!"
-
+        return render_template("index.html")
 if __name__ == "__main__":
         app.run(host="0.0.0.0", port=5000)
